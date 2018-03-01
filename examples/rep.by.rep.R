f36a56bf9c955b891d2a8dcbff912519ff688e71
logrhythm <logrhythm@github.com>
2018-03-01 12:08:16 -0600

    updates

--- examples/rep.by.rep.R
+++ examples/rep.by.rep.R
@@ -6,8 +6,9 @@ setwd("/Users/case0197/Documents/git_BreedR/exmple data")
 #downloaodexample data
 write.csv(read.table("https://raw.githubusercontent.com/austinjcase/BreedR/master/exmple%20data/rep.by.pre.txt", header=T), "rep.by.rep.csv", row.names =F)
 
-x<-read.table("rep.by.rep.txt", head=T)
-x
-out<-rep.by.rep(data = dfa, ids = "lines", plot = "plots" )
+rep.rep<-read.table("rep.by.rep.txt", head=T)
+rep.rep
+
+out<-rep.by.rep(data = rep.rep, ids = "lines", plot = "plots" )
 out
-write.csv(out, "data.csv")
+write.csv(out, "rep.rep.data.csv")
